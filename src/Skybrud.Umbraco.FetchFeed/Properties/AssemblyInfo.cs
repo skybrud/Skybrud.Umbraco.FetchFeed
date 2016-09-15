37ca7f010dc44ea8624588537c2f50c4e24be119
Anders Bjerner <abjerner@skybrud.dk>
2016-09-15 10:57:17 +0200

    Updated AssemblyInfo.cs

--- src/Skybrud.Umbraco.FetchFeed/Properties/AssemblyInfo.cs
+++ src/Skybrud.Umbraco.FetchFeed/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FetchFeed")]
+[assembly: AssemblyTitle("Skybrud.Umbraco.FetchFeed")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skybrud.dk a/s")]
 [assembly: AssemblyProduct("FetchFeed")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
