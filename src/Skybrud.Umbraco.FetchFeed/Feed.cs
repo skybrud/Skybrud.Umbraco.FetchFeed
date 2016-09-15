2c8d06edd73b07f67cbdfe50e0c7809a7d581a28
Anders Bjerner <abjerner@skybrud.dk>
2016-09-15 10:55:58 +0200

    Moving stuff around

---
+++ src/Skybrud.Umbraco.FetchFeed/Feed.cs
@@ -0,0 +1,10 @@
+﻿namespace Skybrud.Umbraco
+{
+    class Feed
+    {
+        public string Alias { get; set; }
+        public string Url { get; set; }
+        public string Path { get; set; }
+        public double Interval { get; set; }
+    }
+}
---
+++ src/Skybrud.Umbraco.FetchFeed/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FetchFeed")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skybrud.dk a/s")]
+[assembly: AssemblyProduct("FetchFeed")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("03c668a6-7138-4b34-9784-a55014a004ad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
